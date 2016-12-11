111d414dfff7eefcf2d774ee4d5205f1c1c7ef3b
Adwin Ying <adwinying@gmail.com>
2016-12-11 19:48:25 +0900

    Add posts

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :items
+  resources :posts
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210160046) do
+ActiveRecord::Schema.define(version: 20161210175917) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
